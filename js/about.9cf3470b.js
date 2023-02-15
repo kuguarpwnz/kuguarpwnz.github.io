291e34ed1d9a07483974ba9305a352b4961e498b
kuguarpwnz <kuguarpwnz@gmail.com>
2023-02-15 16:13:29 +0200

    V2

---
+++ js/about.9cf3470b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktimer"]=self["webpackChunktimer"]||[]).push([[443],{2700:function(e,n,r){r.r(n),r.d(n,{default:function(){return s}});var t=r(3396);function u(e,n){return(0,t.wg)(),(0,t.iD)("div",null,"Settings page TBD")}var i=r(89);const a={},c=(0,i.Z)(a,[["render",u]]);var s=c}}]);
+//# sourceMappingURL=about.9cf3470b.js.map
